7e7f622d40947e2490fd5e4d5ac8b2399c1d98a2
docixpor <119433532+docixpor@users.noreply.github.com>
2023-02-10 15:04:31 +0300

    load...

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,10 @@
 package koschei.config;
 
+import koschei.models.Egg6;
+import koschei.models.Island2;
+import koschei.models.Needle7;
+import koschei.models.Wood3;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 
 public class AppConfig {
-
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
 }
